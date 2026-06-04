6b49310426c4953e1d21608ae71c13cf8d46b902
driasim <driasim@users.noreply.github.com>
2026-06-04 15:38:14 +0000

    [spark-compete] enhance(cli): add --config flag

--- skills-generator/bin/skill.js
+++ skills-generator/bin/skill.js
@@ -4,6 +4,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
+  .option('config', { alias: 'c', type: 'string', description: 'Path to config file' })
   .parse();
 
 console.log("CLI ready");
