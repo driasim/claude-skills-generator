594a958b02e237cec5507ccc0c90505e4be8331f
driasim <driasim@users.noreply.github.com>
2026-06-04 15:37:30 +0000

    [spark-compete] enhance(cli): add --verbose flag

--- skills-generator/bin/skill.js
+++ skills-generator/bin/skill.js
@@ -4,6 +4,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
+  .option('verbose', { alias: 'v', type: 'boolean', description: 'Enable verbose output' })
   .parse();
 
 console.log("CLI ready");
