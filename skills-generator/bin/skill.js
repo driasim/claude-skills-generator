c8cda4454eaadfff38661bcc0d059056be5eec4c
driasim <driasim@users.noreply.github.com>
2026-06-04 15:38:05 +0000

    [spark-compete] enhance(cli): add --format flag

--- skills-generator/bin/skill.js
+++ skills-generator/bin/skill.js
@@ -4,6 +4,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
+  .option('format', { type: 'string', choices: ['json', 'markdown'], description: 'Output format (json/markdown)' })
   .parse();
 
 console.log("CLI ready");
