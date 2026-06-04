22f33b1198d32080c1872148d0988ccdaf495609
driasim <driasim@users.noreply.github.com>
2026-06-04 15:38:28 +0000

    [spark-compete] enhance(cli): add --yes flag

--- skills-generator/bin/skill.js
+++ skills-generator/bin/skill.js
@@ -4,6 +4,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
+  .option('yes', { alias: 'y', type: 'boolean', description: 'Auto-confirm all prompts' })
   .parse();
 
 console.log("CLI ready");
